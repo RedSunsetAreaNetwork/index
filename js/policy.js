7fbe4c8e288e2c21ac594685cf352f3e6f1404cc
Xinzhi Wang <59304676+RSSYLY@users.noreply.github.com>
2022-10-06 00:28:43 +0800

    新增隐私策略对话框

---
+++ js/policy.js
@@ -0,0 +1,96 @@
+//工具
+/*\
+|*|
+|*|  :: cookies.js ::
+|*|
+|*|  A complete cookies reader/writer framework with full unicode support.
+|*|
+|*|  https://developer.mozilla.org/en-US/docs/DOM/document.cookie
+|*|
+|*|  This framework is released under the GNU Public License, version 3 or later.
+|*|  http://www.gnu.org/licenses/gpl-3.0-standalone.html
+|*|
+|*|  Syntaxes:
+|*|
+|*|  * docCookies.setItem(name, value[, end[, path[, domain[, secure]]]])
+|*|  * docCookies.getItem(name)
+|*|  * docCookies.removeItem(name[, path], domain)
+|*|  * docCookies.hasItem(name)
+|*|  * docCookies.keys()
+|*|
+\*/
+
+var docCookies = {
+    getItem: function (sKey) {
+      return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[-.+*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
+    },
+    setItem: function (sKey, sValue, vEnd, sPath, sDomain, bSecure) {
+      if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) { return false; }
+      var sExpires = "";
+      if (vEnd) {
+        switch (vEnd.constructor) {
+          case Number:
+            sExpires = vEnd === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; max-age=" + vEnd;
+            break;
+          case String:
+            sExpires = "; expires=" + vEnd;
+            break;
+          case Date:
+            sExpires = "; expires=" + vEnd.toUTCString();
+            break;
+        }
+      }
+      document.cookie = encodeURIComponent(sKey) + "=" + encodeURIComponent(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : "");
+      return true;
+    },
+    removeItem: function (sKey, sPath, sDomain) {
+      if (!sKey || !this.hasItem(sKey)) { return false; }
+      document.cookie = encodeURIComponent(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "");
+      return true;
+    },
+    hasItem: function (sKey) {
+      return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(sKey).replace(/[-.+*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
+    },
+    keys: /* optional method: you can safely remove it! */ function () {
+      var aKeys = document.cookie.replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "").split(/\s*(?:\=[^;]*)?;\s*/);
+      for (var nIdx = 0; nIdx < aKeys.length; nIdx++) { aKeys[nIdx] = decodeURIComponent(aKeys[nIdx]); }
+      return aKeys;
+    }
+  };
+  
+  console.log("1");
+  // 每次打开网页时调整到预设的主题
+  
+  // 判断是否是初次打开网站，判断储存主题的Cookies是否存在（cookies名为theme_key）
+  if (docCookies.hasItem("policy") == false) {
+    // 不存在则创建cookies，并将值定义为auto
+    document.cookie = "policy=false";
+  };
+  // 将这个变量从cookies赋值
+  var policy_check = docCookies.getItem("policy");
+  // 暗色
+  if (policy_check == "false") {
+  mdui.dialog({
+    title: '<h class="i18n" name="policy">[policy]</h>',
+    modal: 'true',
+    content: '<p class="i18n" name="policyContent">[policyContent]</p>',
+    buttons: [
+      {
+        text: '同意',
+        onClick: function(inst){
+          mdui.snackbar({
+            message: '今后不再显示此信息',
+            position: 'right-top',
+          });
+          docCookies.setItem("policy", "true");
+        }
+      }
+    ]
+  });
+  }
+  else if (policy_check == "true") {
+  
+  }
+  else {
+  
+  };
